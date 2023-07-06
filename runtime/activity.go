9530c786bbd752710124049f8b4b75dc1a54083f
Water.Li <lizongbin@hotmail.com>
2023-07-06 15:16:15 +0800

    Update activity.go

--- runtime/activity.go
+++ runtime/activity.go
@@ -24,4 +24,17 @@ func (j *ActivityJointers) GetInput(name string) *Jointer {
 }
 
 type AbstractActivity struct {
+	Id       string
+	Jointers *ActivityJointers
+	Config   map[string]interface{}
+}
+
+func (a *AbstractActivity) Next(inputValue interface{}, outputName string) {
+	if outputName == "" {
+		outputName = "output"
+	}
+	nextJointer := a.Jointers.GetOutput(outputName)
+	if nextJointer != nil {
+		nextJointer.Push(inputValue)
+	}
 }
