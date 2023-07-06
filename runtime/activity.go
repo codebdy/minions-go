5c2e5b99489b09cc5af061727f72acb117a97e6b
Water.Li <lizongbin@hotmail.com>
2023-07-06 15:11:20 +0800

    Update activity.go

--- runtime/activity.go
+++ runtime/activity.go
@@ -1,4 +1,27 @@
 package runtime
 
+type ActivityJointers struct {
+	inputs  []*Jointer
+	outputs []*Jointer
+}
+
+func (j *ActivityJointers) GetOutput(name string) *Jointer {
+	for _, jointer := range j.outputs {
+		if jointer.Name == name {
+			return jointer
+		}
+	}
+	return nil
+}
+
+func (j *ActivityJointers) GetInput(name string) *Jointer {
+	for _, jointer := range j.inputs {
+		if jointer.Name == name {
+			return jointer
+		}
+	}
+	return nil
+}
+
 type AbstractActivity struct {
 }
