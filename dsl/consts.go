1c336d7a7587a6c55a6c5ce6cf9e0046768d189b
Water.Li <lizongbin@hotmail.com>
2023-07-06 15:05:38 +0800

    完成Jointer

---
+++ dsl/consts.go
@@ -0,0 +1,8 @@
+package dsl
+
+const (
+	ACTIVITY_TYPE_START             = "Start"
+	ACTIVITY_TYPE_END               = "End"
+	ACTIVITY_TYPE_ACTIVITY          = "Activity"
+	ACTIVITY_TYPE_LOGICFLOWACTIVITY = "LogicFlowActivity"
+)
--- runtime/jointer.go
+++ runtime/jointer.go
@@ -1,4 +1,19 @@
 package runtime
 
+type InputHandler = func(inputValue interface{})
+
 type Jointer struct {
+	Id      string
+	Name    string
+	outlets []InputHandler
+}
+
+func (j *Jointer) Push(inputValue interface{}) {
+	for _, outputHandler := range j.outlets {
+		outputHandler(inputValue)
+	}
+}
+
+func (j *Jointer) Connect(inuptHandler InputHandler) {
+	j.outlets = append(j.outlets, inuptHandler)
 }
