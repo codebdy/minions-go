77fc0d70b466d35c848d91a8ddf1aec106630a9a
Water.Li <lizongbin@hotmail.com>
2023-07-07 11:14:38 +0800

    Update activities.go

--- runtime/activities.go
+++ runtime/activities.go
@@ -2,17 +2,11 @@ package runtime
 
 import (
 	"reflect"
-
-	"github.com/codebdy/minions-go/dsl"
 )
 
-var activitiesMap map[string]reflect.Type
+var activitiesMap map[string]reflect.Type = make(map[string]reflect.Type)
 
 //func RegisterActivity[Config any, T Activity[Config]](name string, factory func(meta *dsl.ActivityDefine) *T) {
 func RegisterActivity(name string, activity interface{}) {
 	activitiesMap[name] = reflect.TypeOf(activity)
-}
-
-func makeActivity(meta dsl.ActivityDefine) {
-
 }
