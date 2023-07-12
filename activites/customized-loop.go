e00b54bdd992432b532748aaea1ae010508d17ea
Water.Li <lizongbin@hotmail.com>
2023-07-12 22:24:06 +0800

    backup

--- activites/customized-loop.go
+++ activites/customized-loop.go
@@ -1,1 +1,46 @@
 package activites
+
+import (
+	"fmt"
+
+	"github.com/codebdy/minions-go/runtime"
+)
+
+type CustomizedLoopConfig struct {
+	FromInput bool `json:"fromInput"`
+	Times     int  `json:"times"`
+}
+type CustomizedLoopActivity struct {
+	BaseActivity runtime.BaseActivity[CustomizedLoopConfig]
+}
+
+const CUSTOMIZED_LOOP_PORT_OUTPUT = "output"
+const CUSTOMIZED_LOOP_PORT_FINISHED = "finished"
+
+func init() {
+	runtime.RegisterActivity(
+		"debug",
+		CustomizedLoopActivity{},
+	)
+}
+
+func (l CustomizedLoopActivity) Input(inputValue any) {
+	if l.BaseActivity.GetConfig().FromInput {
+		if inputValue == nil {
+			fmt.Println("CustomizedLoop input is nil")
+		} else {
+			for _, one := range inputValue.([]any) {
+				l.Output(one)
+			}
+		}
+	} else if l.BaseActivity.GetConfig().Times > 0 {
+		for i := 0; i < l.BaseActivity.GetConfig().Times; i++ {
+			l.Output(i)
+		}
+	}
+	l.BaseActivity.Next(inputValue, CUSTOMIZED_LOOP_PORT_FINISHED)
+}
+
+func (l CustomizedLoopActivity) Output(value any) {
+	l.BaseActivity.Next(value, CUSTOMIZED_LOOP_PORT_OUTPUT)
+}
--- activites/loop.go
+++ activites/loop.go
@@ -1,2 +1,46 @@
 package activites
 
+import (
+	"fmt"
+
+	"github.com/codebdy/minions-go/runtime"
+)
+
+type LoopConfig struct {
+	FromInput bool `json:"fromInput"`
+	Times     int  `json:"times"`
+}
+type LoopActivity struct {
+	BaseActivity runtime.BaseActivity[LoopConfig]
+}
+
+const LOOP_PORT_OUTPUT = "output"
+const LOOP_PORT_FINISHED = "finished"
+
+func init() {
+	runtime.RegisterActivity(
+		"debug",
+		LoopActivity{},
+	)
+}
+
+func (l LoopActivity) Input(inputValue any) {
+	if l.BaseActivity.GetConfig().FromInput {
+		if inputValue == nil {
+			fmt.Println("Loop input is nil")
+		} else {
+			for _, one := range inputValue.([]any) {
+				l.Output(one)
+			}
+		}
+	} else if l.BaseActivity.GetConfig().Times > 0 {
+		for i := 0; i < l.BaseActivity.GetConfig().Times; i++ {
+			l.Output(i)
+		}
+	}
+	l.BaseActivity.Next(inputValue, LOOP_PORT_FINISHED)
+}
+
+func (l LoopActivity) Output(value any) {
+	l.BaseActivity.Next(value, LOOP_PORT_OUTPUT)
+}
