443abbe9367363f374001a416b27cda2c35f69fe
Water.Li <lizongbin@hotmail.com>
2023-07-14 17:22:02 +0800

    Update delay.go

--- activites/delay.go
+++ activites/delay.go
@@ -1,1 +1,29 @@
 package activites
+
+import (
+	"context"
+	"time"
+
+	"github.com/codebdy/minions-go/runtime"
+)
+
+type DelayConfig struct {
+	//单位是秒
+	Time int `json:"time"`
+}
+type DelayActivity struct {
+	Activity runtime.Activity[DelayConfig]
+}
+
+func init() {
+	runtime.RegisterActivity(
+		"delay",
+		DelayActivity{},
+	)
+}
+
+func (d *DelayActivity) Input(inputValue any, ctx context.Context) {
+	config := d.Activity.GetConfig()
+	time.Sleep(time.Duration(config.Time) * time.Second)
+	d.Activity.Output(inputValue, ctx)
+}
